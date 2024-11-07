427973ad231b8bb337d7f1a1763633943b6b7d4a
isaacCoder5 <683issaacopoku@gmail.com>
2024-11-07 01:09:04 +0000

    Initial commit

---
+++ assignment.js
@@ -0,0 +1,18 @@
+//Define a function to calculate the Area that takes two parameters: width and height.
+
+function calculateArea(width, height) {
+    const area = width * height;
+    return area;
+}
+
+//Call the function with values 
+
+const result = calculateArea(10, 5);
+
+//Printing 
+
+console.log("The area of the rectangle is:", result);
+
+
+git config --global user.email "683issaacopoku@gmail.com"
+git config --global user.name "isaacCoder5"
